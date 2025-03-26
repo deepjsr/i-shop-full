8fa32032c8eb97a6da907bd5ba2edf101a5499a9
deepjsr <sarma.deepjyoti481@gmail.com>
2025-03-26 10:09:14 +0530

    set firebase config with env

--- src/context/firebase.jsx
+++ src/context/firebase.jsx
@@ -6,21 +6,19 @@ import {
   getAuth,
   GoogleAuthProvider,
   signInWithPopup,
-  signInWithRedirect,
   signOut,
 } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCIoRE9fzIAHrDHOBcVZEvEQdNSO12gl0",
-  authDomain: "i-shopful.firebaseapp.com",
-  projectId: "i-shopful",
-  storageBucket: "i-shopful.firebasestorage.app",
-  databaseURL:
-    "https://i-shopful-default-rtdb.asia-southeast1.firebasedatabase.app",
-  messagingSenderId: "1040309158633",
-  appId: "1:1040309158633:web:bca38ca8f6ea86093ba678",
-  measurementId: "G-1E0YXQT79Z",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
